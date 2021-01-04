d97807bd46968f5095ae503ae82c5a54ccb255d5
kavinsurya <kavinsurya002@gmail.com>
2021-01-04 20:20:00 +0530

    app build

---
+++ build/precache-manifest.0fd2a3699ccef4b55f0fc2dee7533c28.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75013661f14b2252ca136e0f9cb6671f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "537bd6684434b83cf59c",
+    "url": "/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "7a60bfc9aa9738ef5501",
+    "url": "/static/css/main.54d43338.chunk.css"
+  },
+  {
+    "revision": "537bd6684434b83cf59c",
+    "url": "/static/js/2.b5c5ee54.chunk.js"
+  },
+  {
+    "revision": "b29fa9d568ccdfac989d0d2ab5fd3892",
+    "url": "/static/js/2.b5c5ee54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a60bfc9aa9738ef5501",
+    "url": "/static/js/main.a2ede09f.chunk.js"
+  },
+  {
+    "revision": "6c6cdccbc65d0104dabf",
+    "url": "/static/js/runtime-main.6864bc36.js"
+  }
+]);
