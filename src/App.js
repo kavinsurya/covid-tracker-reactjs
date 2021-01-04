6d23fc101a30225cfd538771de5e4c6796701cf8
kavinsurya <kavinsurya002@gmail.com>
2021-01-04 17:49:46 +0530

    info box

--- src/App.js
+++ src/App.js
@@ -1,23 +1,111 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+
+import { MenuItem, Select, FormControl, Card } from "@material-ui/core";
+
+import InfoBox from "./InfoBox";
+import Map from "./Map";
+import Table from "./Table";
+import LineGraph from "./LineGraph"
+
+import sortedData from "./utils"
 
 function App() {
+  const [country, setInputCountry] = useState("worldwide");
+  const [countries, setCountries] = useState([]);
+  const [countryInfo, setCountryInfo] = useState({});
+  const [tableData,setTableData] =useState([])
+  console.log(country);
+  console.log("countryInfo", countryInfo);
+
+  useEffect(() => {
+    fetch("https://disease.sh/v3/covid-19/all")
+      .then((response) => response.json())
+      .then((data) => {
+        setCountryInfo(data);
+      });
+  }, []);
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
+          setCountries(countries);
+
+          const sortData= sortedData(data)
+          setTableData(sortData)
+        });
+    };
+    getCountriesData();
+  }, []);
+
+  const getCountryCode = async (event) => {
+    let countryCode = event.target.value;
+
+    const url =
+      countryCode === "worldwide"
+        ? "https://disease.sh/v3/covid-19/all"
+        : `https://disease.sh/v3/covid-19/countries/${countryCode}`;
+    await fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setInputCountry(countryCode);
+        console.log(data);
+        setCountryInfo(data);
+      });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app__left">
+        <div className="app__header">
+          <h1>Covid19-tracker</h1>
+          <FormControl className="app__dropdown">
+            <Select
+              variant="outlined"
+              onChange={getCountryCode}
+              value={country}
+            >
+              <MenuItem value="worldwide">Worldwide</MenuItem>
+              {countries.map((country, index) => (
+                <MenuItem value={country.value} key={index}>
+                  {country.name}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </div>
+
+        <div className="app__stats">
+          <InfoBox
+            title="Coronavirus Cases"
+            cases={countryInfo.todayCases}
+            total={countryInfo.cases}
+          />
+          <InfoBox
+            title="Recovered"
+            cases={countryInfo.todayRecovered}
+            total={countryInfo.recovered}
+          />
+          <InfoBox
+            title="Deaths"
+            cases={countryInfo.todayDeaths}
+            total={countryInfo.deaths}
+          />
+        </div>
+        <Map />
+      </div>
+      <Card className="app__right">
+      <h3 className="card-header">Live cases in countries</h3>
+        <Table countries={tableData} />
+        <h3 className="card-header" > Worldwide new</h3>
+        <LineGraph/>
+        
+      </Card>
     </div>
   );
 }
---
+++ src/Map.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Map =() =>{
+    return(
+        <div className="map">
+
+        </div>
+    )
+}
+
+
+export default Map
