7b576a8224d3a0315453ffbcfcc9f15a53470fad
kavinsurya <kavinsurya002@gmail.com>
2021-01-04 20:14:46 +0530

    Table

--- src/Table.js
+++ src/Table.js
@@ -1,24 +1,18 @@
 import React from "react";
 import "./Table.css";
-// import numeral from "numeral";
+import numeral from "numeral";
 
 function Table({ countries }) {
   return (
     <div className="table">
-   
-      <table style={{width:"100%"}}>
-       
-        <tbody>
-      {countries.map((country,index) => (
-        <tr key={index}>
+      {countries.map((country) => (
+        <tr>
           <td>{country.country}</td>
           <td>
-            <strong>{(country.cases)}</strong>
+            <strong>{numeral(country.cases).format("0,0")}</strong>
           </td>
         </tr>
       ))}
-      </tbody>
-      </table>
     </div>
   );
 }
